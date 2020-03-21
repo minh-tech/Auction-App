81a1a44f615cd51d785e3502daba2fa942457d45
minh <hoangminh1612@gmail.com>
2020-03-21 12:44:57 -0700

    Add new code

--- Client-app/App.js
+++ Client-app/App.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Keyboard,
+  FlatList
+} from 'react-native';
+
+import FilteredItems from './components/FilteredItems';
+import HeaderApp from './components/HeaderApp';
 
 export default function App() {
+  
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addSearchHandler = (searchItem) => {
+    Keyboard.dismiss();
+    setFilteredItems(currentItems => [
+      ...currentItems,
+      { id: Math.random().toString(), value: searchItem }
+    ]);
+    setIsAddMode(false);
+  }
+
+  const removeItemHandler = itemId => {
+    setFilteredItems(currentItems => {
+      return currentItems.filter((item) => item.id !== itemId);
+    });
+  }
+
+  const cancelSearchHandler = () => {
+    setIsAddMode(false);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.Screen}>
+      <Text style={styles.CompanyName}>Online Auction</Text>
+      <Button title="New Search" onPress={() => setIsAddMode(true)} />
+      <HeaderApp
+        visible={isAddMode}
+        onAddSearch={addSearchHandler}
+        onCancelSearch={cancelSearchHandler}
+      />
+      <FlatList
+        data={filteredItems}
+        renderItem={itemData =>
+          <FilteredItems
+            id={itemData.item.id}
+            onDelete={removeItemHandler}
+            title={itemData.item.value}
+          /> 
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  Screen: {
+    padding: 50,
+  },
+  CompanyName: {
+    fontWeight: 'bold',
+    color: 'blue',
+    fontSize: 25,
+    textAlign: 'center',
   },
 });
